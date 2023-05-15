041f40dca6efefbdee9c0341ebff08b2dc61c330
thakuramit962 <thakuramit962@gmail.com>
2023-05-15 17:03:30 +0530

    issue in auto logout on token expire resolved

--- src/web-routes/protected-route-auth-check.tsx
+++ src/web-routes/protected-route-auth-check.tsx
@@ -5,6 +5,7 @@ import {RootState} from "../store/store";
 import axios from "axios";
 import {updateAuth} from "../slices/auth-slice";
 import {User} from "../models/user";
+import API from "../api";
 
 
 export default function ProtectedRouteAuthCheck() {
@@ -13,14 +14,13 @@ export default function ProtectedRouteAuthCheck() {
     const location = useLocation()
     const accessToken = useSelector((state: RootState) => state.userAuth.accessToken)
 
-    axios.defaults.headers.common['Content-Type'] = `application/json`
-    axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`
-    axios.defaults.headers.common['crossorigin'] = `anonymous`
+    API.defaults.headers.common['Content-Type'] = `application/json`
+    API.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`
+    API.defaults.headers.common['crossorigin'] = `anonymous`
 
-    axios.interceptors.response.use(function (response) {
+    API.interceptors.response.use(function (response) {
         if (response.data.statuscode == 401) {
             dispatch(updateAuth({isLoggedIn: false, accessToken: '', currentUser: {} as User}))
-            console.log('logout now amit')
         }
         return response;
     }, function (error) {
